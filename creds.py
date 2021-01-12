3caeb86af713eb7fb5f759589507d85e929188db
Venkata Shravan <kandadishravan@gmail.com>
2021-01-12 14:01:30 +0530

    Fix exit

--- creds.py
+++ creds.py
@@ -1,4 +1,4 @@
-email = "kandadishravan@gmail.com" #Replace with your email
-passwd = "kvshravan1@"				#Type in your passwd
+email = "abc@gmail.com" #Replace with your email
+passwd = "passwd"				#Type in your passwd
 #***#
 NotHSCourse = "Language and Society" #Fill in the HS course you're NOT taking part in
